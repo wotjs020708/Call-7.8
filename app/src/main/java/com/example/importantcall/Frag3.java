9bef0ae6e1372f6ac0df9db4f0d4bea9ec1e2b3f
wotjs020708 <wotjs020708@naver.com>
2020-07-05 23:55:49 +0900

    commmit

--- app/src/main/java/com/example/importantcall/Frag3.java
+++ app/src/main/java/com/example/importantcall/Frag3.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,13 +14,31 @@ import androidx.fragment.app.Fragment;
 
 public class Frag3 extends Fragment {
 
+    Button btn_inp;
+    EditText ed_inp;
+    TextView text_meg;
+    private String str;
+
     private View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         view = inflater.inflate(R.layout.frag3, container, false);
+        btn_inp = (Button) view.findViewById(R.id.btn_inp);
+        ed_inp = (EditText) view.findViewById(R.id.ed_inp);
+        text_meg = (TextView) view.findViewById(R.id.text_meg);
+
+        btn_inp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               str = ed_inp.getText().toString();
+               text_meg.setText(str);
+               ed_inp.setText("");
+
+
+            }
+        });
 
         return  view;
 
