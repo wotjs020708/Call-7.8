07174f3751500f9ddebff1bfb1259963f8a3d0ae
wotjs020708 <wotjs020708@naver.com>
2020-07-08 10:58:11 +0900

    commmit

--- app/src/main/java/com/example/importantcall/Frag1.java
+++ app/src/main/java/com/example/importantcall/Frag1.java
@@ -1,10 +1,16 @@
 package com.example.importantcall;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,30 +18,72 @@ import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 public class Frag1 extends Fragment {
-
+    ToggleButton toggleButton_start;
     private View view;
     ImageView imageView;
     SwitchCompat switchCompat;
+    TelephonyManager telephonyManager;
+    private boolean set_btn = false;
+    PhoneStateListener phoneStateListener = new PhoneStateListener(){
+        @Override
+        public void onCallStateChanged(int state, String phoneNumber) {
+            switch(state)
+            {
+                case TelephonyManager.CALL_STATE_IDLE :
+                    Toast.makeText(getActivity(),"통화x",Toast.LENGTH_SHORT).show();
+                    break;
+                case TelephonyManager.CALL_STATE_RINGING :
+                    Toast.makeText(getActivity(),"폰이울림",Toast.LENGTH_SHORT).show();
+                    break;
 
+                case TelephonyManager.CALL_STATE_OFFHOOK :
+                    Toast.makeText(getActivity(),"통화중",Toast.LENGTH_SHORT).show();
+                    break;
+                default:break;
+
+            }
+        }
+    };
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.frag1, container, false);
         imageView = (ImageView) view.findViewById(R.id.imageView);
-        switchCompat = (SwitchCompat) view.findViewById(R.id.switchButton);
-
+        //switchCompat = (SwitchCompat) view.findViewById(R.id.switchButton);
+        toggleButton_start = (ToggleButton) view.findViewById(R.id.toggleButton_start);
         imageView.setImageResource(R.drawable.ic_baseline_blur_on_24);
-        switchCompat.setOnClickListener(new View.OnClickListener() {
+        toggleButton_start.setOnClickListener(new View.OnClickListener() {
             @Override
+            public void onClick(View view) {
+                if(toggleButton_start.isChecked()){
+                    set_btn = true;
+                        if (set_btn == true){
+                            imageView.setImageResource(R.drawable.ic_baseline_blur_off_24);
+                            telephonyManager = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+                            telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+                        }
+                } else {
+                        set_btn = false;
+                        imageView.setImageResource(R.drawable.ic_baseline_blur_on_24);
+
+                    
+                }
+                }
+        });
+        /*switchCompat.setOnClickListener(new View.OnClickListener() {
+            @Override
+
             public void onClick(View view) {
                 if (switchCompat.isChecked()){
                     imageView.setImageResource(R.drawable.ic_baseline_blur_off_24);
+                    telephonyManager=(TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+                    telephonyManager.listen(phoneStateListener,PhoneStateListener.LISTEN_CALL_STATE);
                 } else {
                     imageView.setImageResource(R.drawable.ic_baseline_blur_on_24);
                 }
             }
-        });
+        });*/
 
         return  view;
 
--- app/src/main/java/com/example/importantcall/Frag3.java
+++ app/src/main/java/com/example/importantcall/Frag3.java
@@ -1,15 +1,22 @@
 package com.example.importantcall;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 public class Frag3 extends Fragment {
@@ -18,16 +25,43 @@ public class Frag3 extends Fragment {
     EditText ed_inp;
     TextView text_meg;
     private String str;
-
+    private String str1,str2;
+    static final int SMS_PERMISSION=1;
     private View view;
+
+
+    private void HiSendMessage(String phoneNo, String sms )
+    {
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        int permssionCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.SEND_SMS);
+        if(permssionCheck == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(getActivity(), "SMS 수신권한 있음", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getActivity(), "SMS 수신권한 없음", Toast.LENGTH_SHORT).show();
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.SEND_SMS)){
+
+                Toast.makeText(getActivity(), "SMS권한이 필요합니다", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(getActivity(), new String[]{ Manifest.permission.SEND_SMS}, SMS_PERMISSION);
+            }
+            else{ActivityCompat.requestPermissions(getActivity(), new String[]{ Manifest.permission.SEND_SMS}, SMS_PERMISSION);
+            }
+        }
+
         view = inflater.inflate(R.layout.frag3, container, false);
         btn_inp = (Button) view.findViewById(R.id.btn_inp);
         ed_inp = (EditText) view.findViewById(R.id.ed_inp);
         text_meg = (TextView) view.findViewById(R.id.text_meg);
+        str1="01050135766"; //"전화번호"
+
 
         btn_inp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +69,8 @@ public class Frag3 extends Fragment {
                str = ed_inp.getText().toString();
                text_meg.setText(str);
                ed_inp.setText("");
-
+                str2 = str;
+                HiSendMessage(str1,str2);
 
             }
         });
